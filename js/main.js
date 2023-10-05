a12db1654d32e75df9b6424762e64bfeda3e21a6
jhj <kjgrt33@naver.com>
2023-10-05 17:31:35 +0900

    last

--- js/main.js
+++ js/main.js
@@ -1,64 +1,187 @@
-// nav
-$(function () {
-  let arr = [
-    $("section").eq(0).offset().top,
-    $("section").eq(1).offset().top,
-    $("section").eq(2).offset().top,
-    $("section").eq(3).offset().top,
-  ];
-  console.log(arr);
-
-  $("nav ul li a").click(function (e) {
-    e.preventDefault();
-    let i = $(this).parent().index();
-    $("html, body").animate({ scrollTop: arr[i] - 60 }, 1000);
-  });
-
-  //popup
-  //열기
-  $(".portfolio .con li a").click(function () {
-    let i = $(this).parent().index();
-    if (i >= 3) {
-      $("#popup00 > div").css({ display: "none" });
-      $("#popup00 > div")
-        .eq(i - 3)
-        .css({ display: "block" });
-      $("body").css({ overflow: "hidden" });
-      return false;
-    }
-  });
-  // 나중에 클론페이지 없이 각 리스트마다 레이어팝업 뜨게 할때 활용!!!
-  /* $(".portfolio .con li a").click(function (e) {
-    e.preventDefault();
-    let i = $(this).parent().index();
-    
-      $("#popup00 > div").css({ display: "none" });
-      $("#popup00 > div")
-        .eq(i)
-        .css({ display: "block" });
-      $("body").css({ overflow: "hidden" });
-   
-  }); */
-
-  //닫기
-  $(".btn_close").click(function (e) {
-    e.preventDefault();
-    $("#popup00 > div").css({ display: "none" });
-    $("body").css({ overflow: "visible" });
-  });
-
-  // TOP 버튼
-  window.onscroll = function () {
-    let ht = document.documentElement.scrollTop;
-    console.log(ht);
-    if (ht > 1000) {
-      document.getElementsByClassName("btn_top")[0].style.opacity = "1";
-    } else {
-      document.getElementsByClassName("btn_top")[0].style.opacity = "0";
-    }
-  };
-  $(".btn_top").click(function (e) {
-    e.preventDefault();
-    $("html,body").animate({ scrollTop: "0" }, 1000);
-  });
+//nav
+//열기
+$("#navi_view").click(function (e) {
+  e.preventDefault();
+  $("#navi").css({ display: "block" });
 });
+
+//닫기
+$("#close").click(function (e) {
+  e.preventDefault();
+  $("#navi").css({ display: "none" });
+  $(".navi2").css({ display: "none" });
+});
+
+//menu1
+//열기 닫기
+/* let status = true;
+$("#menu1").click(function (e) {
+  e.preventDefault();
+  if (status) {
+    $("#visit").css({ display: "block" });
+  } else {
+    $("#visit").css({ display: "none" });
+  }
+  status = !status;
+}); */
+
+//menu2
+//열기 닫기
+/* $("#menu2").click(function (e) {
+  e.preventDefault();
+  if (status) {
+    $("#find").css({ display: "block" });
+  } else {
+    $("#find").css({ display: "none" });
+  }
+  status = !status;
+}); */
+
+//menu3
+//열기 닫기
+/* $("#menu3").click(function (e) {
+  e.preventDefault();
+  if (status) {
+    $("#now").css({ display: "block" });
+  } else {
+    $("#now").css({ display: "none" });
+  }
+  status = !status;
+}); */
+
+$(".navi2").css({ display: "none" });
+$(".gnb ul li").click(function (e) {
+  e.preventDefault();
+  let i = $(this).index();
+  console.log(i);
+  if (
+    $(this).find("a").hasClass("on") &&
+    $(".navi2").eq(i).css("display") == "block"
+  ) {
+    $(".navi2").stop().slideUp();
+    $(".gnb ul li a").removeClass("on");
+  } else {
+    $(".navi2").eq(i).stop().slideDown();
+    $(".gnb ul li a").removeClass("on");
+    $(".gnb ul li").eq(i).find("a").addClass("on");
+  }
+});
+
+/* $(".gnb ul li").click(function (e) {
+  e.preventDefault();
+  let i = $(this).index();
+  console.log(i);
+  if (
+    $(this).find("a").hasClass("on") &&
+    $(".navi2").eq(i).css("display") == "block"
+  ) {
+    $(".navi2").animate({ left: "-100%" }, 1000);
+
+    $(".gnb ul li a").removeClass("on");
+  } else {
+    $(".navi2").animate({ left: "680px" }, 1000, function () {
+      $(".navi2").css({ zIndex: "10" });
+    });
+    $(".gnb ul li a").removeClass("on");
+    $(".gnb ul li").eq(i).find("a").addClass("on");
+  }
+}); */
+
+// visual
+const video = document.getElementById("myVideo");
+const pauseButton = document.getElementById("pauseButton");
+
+// 버튼을 클릭할 때 동영상을 일시 정지 또는 다시 재생
+pauseButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  pauseButton.classList.toggle("on");
+  if (video.paused) {
+    video.play(); // 일시 정지된 동영상을 재생
+  } else {
+    video.pause(); // 동영상을 일시 정지
+  }
+});
+
+// IN THE NEWS
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// LOUVRE AT HOME
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  //centeredSlides: true,
+  slidesPerGroup: 1,
+  slidesPerGroupAuto: true,
+  slidesOffsetBefore: 100,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    //clickable: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// YOU WANT TO
+let img_src = $("#b_pic img").attr("src");
+
+$(".b_list li").hover(
+  function () {
+    let i = $(this).index(); // index는 0부터 시작
+
+    $("#b_pic img")
+      .attr("src", img_src.replace(img_src.substr(-5, 1), i + 2)) // index는 0부터 시작이므로 +2로
+      .attr("alt", "banner" + i);
+  },
+  function () {
+    $("#b_pic img").attr("src", "images/banner1.jpg").attr("alt", "banner1");
+  }
+);
+
+/* INSTAGRAM */
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  //centeredSlides: true, 이거 잠시 주석
+  slidesPerGroup: 1,
+  slidesPerGroupAuto: true,
+  slidesOffsetBefore: 100,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// TOP 버튼
+window.onscroll = function () {
+  let ht = document.documentElement.scrollTop;
+  console.log(ht);
+  if (ht > 50) {
+    document.getElementsByClassName("btn_top")[0].style.opacity = "1";
+  } else {
+    document.getElementsByClassName("btn_top")[0].style.opacity = "0";
+  }
+};
+document.getElementsByClassName("btn_top")[0].onclick = function () {
+  document.documentElement.scrollTop = "0";
+};
